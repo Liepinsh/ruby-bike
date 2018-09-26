dd935bf6511e8bd97e73df9b3272c10e4968bbc4
Liepinsh <edgars.liepajnieks@gmail.com>
2018-09-27 00:35:39 +0300

    second bike app commit 27.09.2018

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,84 @@
 class Reservation < ApplicationRecord
     belongs_to :employee
     belongs_to :bike
+    
+    validate :time
+    validate :takens
+
+    def taken
+        if bike.ride.include? "false"
+            show_time
+        else
+            "It's available"
+        end
+    end
+
+    def show_time
+        "#{from} - #{till}"
+    end
+
+    def time
+        if from == till
+            errors.add(:till, "and From can't be the same")
+        end
+    end
+
+    def takens
+        if bike.ride.include? "false"
+            errors.add(:bike_id, "is reserved. Pick another one or wait from #{from} till #{till} #{bike_id} ")
+        end
+    end
 end
+
+    
+
+# class Reservation < ApplicationRecord
+#     belongs_to :employee
+#     belongs_to :bike
+
+#     def taken
+#         if ride.include? "false"
+#             show_time
+#         end
+#     end
+
+#     def show_time
+#         "#{from} - #{till}"
+#     end
+# end
+
+# def takens
+#     if bike.ride.include? "false"
+#         errors.add(:bike_id, "is reserved. Pick another one or wait from #{from} till #{till} ")
+#     end
+# end
+
+# def takens
+#     if bike.ride == @reservations.reservation.bike.ride
+#         errors.add(:bike_id, "is reserved. Pick another one or wait from #{from} till #{till} ")
+#     end
+# end
+
+# def takens
+#     if bike.ride == reservation.bike.ride
+#         errors.add(:bike_id, "is reserved.")
+#     end
+# end
+
+# def takens
+#     if bike_id == 2
+#         errors.add(:bike_id, "sdddddddddddd")
+#     end
+# end
+
+# def taken
+#     if bike.ride.include? "false"
+#         show_time
+#     end
+# end
+
+# def taken
+#     if bike_id.value == "Bee false"
+#         errors.add(:bike_id, "and Guest score can't be the same")        end
+#     end
+# end
