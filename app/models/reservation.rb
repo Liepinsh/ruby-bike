0921c6e0834c7cb2205910ff47c656fdc8449a95
Liepinsh <edgars.liepajnieks@gmail.com>
2018-10-01 15:22:46 +0300

    final bike app 01.10.2018

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -10,21 +10,13 @@ class Reservation < ApplicationRecord
         end
     end
 
-    # validate :availability 
+    validate :availability 
 
-    # def availability
-    #     @bikes.each do |bike|
-    #         if self > 0
-
-    #             errors.add(:bike_id, "Bike is not available at this time")
-    #         end
-    # end
-
-    # def availability
-    #     if Bike.where(name: self.bike_id).where("starting <= ?", self.starting).where("till >= ?", self.starting).size > 0 ||
-    #         Bike.where(name: self.bike_id).where("starting <= ?", self.till).where("till >= ?", self.till).size > 0
+    def availability
+        if Reservation.where(bike_id: self.bike_id).where("starting < ?", self.starting).where("till >= ?", self.starting).size > 0 ||
+            Reservation.where(bike_id: self.bike_id).where("starting < ?", self.till).where("till >= ?", self.till).size > 0
     
-    #         errors.add(:bike_id, "Bike is not available at this time")
-    #     end
-    # end
+            errors.add(:bike_id, "#{bike.name} is not available at this time")
+        end
+    end
 end
