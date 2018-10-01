7e59d63a7dc8a286f292d520be0bbdda8a5df5eb
Liepinsh <edgars.liepajnieks@gmail.com>
2018-10-01 15:07:02 +0300

    seventh bike app commit 01.10.2018

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,24 +6,24 @@ class Reservation < ApplicationRecord
 
     def time
         if starting == till
-            errors.add(:till, "and From can't be the same")
+            errors.add(:till, "and Starting can't be the same")
         end
     end
 
     # validate :availability 
 
     # def availability
-    #     if Bike.where(bike_id: self.bike_id).where("starting <= ?", self.starting).where("till >= ?", self.starting).size > 0 ||
-    #         Bike.where(bike_id: self.bike_id).where("starting <= ?", self.till).where("till >= ?", self.till).size > 0
-    
-    #         errors.add(:bike_id, "Bike is not available at this time")
-    #     end
+    #     @bikes.each do |bike|
+    #         if self > 0
+
+    #             errors.add(:bike_id, "Bike is not available at this time")
+    #         end
     # end
 
     # def availability
-    #     if Bike.where(bike: self).where("starting <= ?", self.starting).where("till >= ?", self.starting).size > 0 ||
-    #         Bike.where(bike, self.bike).where("starting <= ?", self.till).where("till >= ?", self.till).size > 0
-
+    #     if Bike.where(name: self.bike_id).where("starting <= ?", self.starting).where("till >= ?", self.starting).size > 0 ||
+    #         Bike.where(name: self.bike_id).where("starting <= ?", self.till).where("till >= ?", self.till).size > 0
+    
     #         errors.add(:bike_id, "Bike is not available at this time")
     #     end
     # end
